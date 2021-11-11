da1bcd3524503a1d0bcdfbb16a6a0c8de709ffa5
PilotMv <91613636+PilotMv@users.noreply.github.com>
2021-11-11 22:25:04 +0200

    servos

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,34 @@
 #include <Arduino.h>
+#include <Servo.h>
 
 //Who's your daddy
 
 int current_task = 0;
 
 #define led_pin 13
-#define MC_enable_A 9
-#define MC_enable_B 10
+#define MC_enable_A 11
+#define MC_enable_B 13
 #define M_1_dir_1 2
 #define M_1_dir_2 3
 #define M_2_dir_1 4
 #define M_2_dir_2 5
 #define drivingspeed 255 // 0-255
 
+//servo setup
+Servo servosingle;
+Servo servodouble;
+#define singleservopin 9 
+int servosinglepos = 0;
+int servodoublepos = 0;
+
+
 void setup() {
   // put your setup code here, to run once:
-
-  pinMode(led_pin, OUTPUT);
-
   pinMode(MC_enable_A, OUTPUT);
   pinMode(M_1_dir_1, OUTPUT);
   pinMode(M_1_dir_2, OUTPUT);
 
-
-
+  servosingle.attach(singleservopin);
 }
 
 void loop() {
@@ -33,6 +38,7 @@ void loop() {
     digitalWrite(M_1_dir_2, LOW);
     while (true){
       analogWrite(MC_enable_A, drivingspeed);
+      
     }
   }
 
