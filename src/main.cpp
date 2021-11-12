5be87555d7f6d2e68fe1eb6d65b3e81aab001053
PilotMv <91613636+PilotMv@users.noreply.github.com>
2021-11-12 02:33:19 +0200

    servos and task 2 and part of task 3

--- src/main.cpp
+++ src/main.cpp
@@ -3,33 +3,46 @@
 
 //Who's your daddy
 
+#define interrupt_sensor 2
+
 int current_task = 0;
 
-#define led_pin 13
 #define MC_enable_A 11
-#define MC_enable_B 13
-#define M_1_dir_1 2
-#define M_1_dir_2 3
-#define M_2_dir_1 4
-#define M_2_dir_2 5
+#define M_1_dir_1 3
+#define M_1_dir_2 4
 #define drivingspeed 255 // 0-255
 
 //servo setup
 Servo servosingle;
 Servo servodouble;
-#define singleservopin 9 
-int servosinglepos = 0;
-int servodoublepos = 0;
+#define singleservopin 9
+#define secondservopin 10
+// int servosinglepos = 0;
+// int servodoublepos = 0;
+#define dropoffpointservo1 15
+#define dropoffpointservo2 -15
+volatile int station = 0;
+
+void inter(){
+  if (current_task != 0){
+    station++;
+  }
+}
 
 
 void setup() {
+
+  delay(5000);
   // put your setup code here, to run once:
   pinMode(MC_enable_A, OUTPUT);
   pinMode(M_1_dir_1, OUTPUT);
   pinMode(M_1_dir_2, OUTPUT);
 
   servosingle.attach(singleservopin);
-}
+  servodouble.attach(secondservopin);
+  // Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(2), inter, FALLING);
+  }
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -38,10 +51,31 @@ void loop() {
     digitalWrite(M_1_dir_2, LOW);
     while (true){
       analogWrite(MC_enable_A, drivingspeed);
-      
     }
   }
+  if (current_task == 1){
+    digitalWrite(M_1_dir_1, HIGH);
+    digitalWrite(M_1_dir_2, LOW);
+    while (station < 2){
+      analogWrite(MC_enable_A, drivingspeed);
+    }
+    analogWrite(MC_enable_A, 0);
+  }
+  if (current_task == 2){
+    digitalWrite(M_1_dir_1, HIGH);
+    digitalWrite(M_1_dir_2, LOW);
+    while (station < 2){
+      analogWrite(MC_enable_A, drivingspeed);
+    }
+    analogWrite(MC_enable_A, 0);
+    delay(1500);
+    servosingle.write(dropoffpointservo2);
+    delay(2000);
 
+
+    
+    
+  }
 
 
 
